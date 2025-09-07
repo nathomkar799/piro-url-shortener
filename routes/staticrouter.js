32e188fa37a0c1eb5eeb2ea2032fe29d238842e3
Omkar Nath <nathomkar799@gmail.com>
2025-09-07 10:25:28 +0530

    Added a highlighting to the whole row of the freshly generatede url

--- routes/staticrouter.js
+++ routes/staticrouter.js
@@ -6,9 +6,15 @@ const router = express.Router();
 
 router.get('/', async (req,res)=> {
     try {
+        if (!req.user) {
+            return res.redirect('/login'); // or any safe fallback
+          }
         const urls = await URL.find({ createdBy: req.user._id });
         const latest = req.query.latest || null; // latest generated URL, if any
-        res.render("home", { urls, latest });
+        const baseURL = req.protocol + '://' + req.get('host');
+
+
+        res.render("home", { urls, latest ,baseURL});
       } catch (err) {
         console.error(err);
         res.status(500).send("Server Error");
